d5d16932f846047bf3fd72aa89111abfba61d5dd
selvasaravanan123 <selvasaravanan123@gmail.com>
2017-03-13 11:08:51 +0530

    Assignment 5.2

---
+++ Assignment 5.2/app.js
@@ -0,0 +1,11 @@
+var obj = {
+	name : "Selva",
+	age : 24,
+	dob : new Date("16 Aug,1992"),
+	pob : "Cuddalore"
+}
+
+console.log("Name : " +obj.name);
+console.log("age : " +obj.age);
+console.log("dob : " +obj.dob);
+console.log("pob : " +obj.pob);
