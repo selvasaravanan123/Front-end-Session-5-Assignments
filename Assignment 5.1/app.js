6e3baf2b91c72d64685469287cdf88e31dbbca75
selvasaravanan123 <selvasaravanan123@gmail.com>
2017-03-13 16:28:25 +0530

    Assignment 5.1

---
+++ Assignment 5.1/app.js
@@ -0,0 +1,6 @@
+
+var name = "Selvasaravanan"; 
+var age  = 24; 
+var birthplace = "TamilNadu";
+var dob = "16 August 1992";
+console.log("Name : "+ name + ", Age :  "+ age +", Place of birth : " + birthplace + " and Date of birth : " + dob ); 
